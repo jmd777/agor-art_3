312c1e408e636c5ce811221d2d7a90488ef4bcc9
Gass-Dev <gwenaelle.fouquet@gmail.com>
2019-12-17 21:06:01 +0100

    Gwen Ajout de Description / Groupes Discussions / Icons / Relier les interface ajoutées entres elles

---
+++ agor art Ayite/DiscussionsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiscussionsViewController.swift
+//  Projet_AGOR_ART
+//
+//  Created by JMD on 13/12/2019.
+//  Copyright © 2019 JMD. All rights reserved.
+//
+
+import UIKit
+
+class DiscussionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
